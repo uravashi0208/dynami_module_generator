0e63716b7e5b46170ca546bf010fa4013ad09082
uravashi0208 <zeel129patel@gmail.com>
2025-09-23 09:51:07 +0200

    first commit

--- fe_angular/playwright.config.ts
+++ fe_angular/playwright.config.ts
@@ -24,7 +24,7 @@ export default defineConfig({
   /* Shared settings for all the projects below. See https://playwright.dev/docs/api/class-testoptions. */
   use: {
     /* Base URL to use in actions like `await page.goto('/')`. */
-    baseURL: 'http://localhost:4200',
+    baseURL: 'https://dynami-module-generator.onrender.com',
 
     /* Collect trace when retrying the failed test. See https://playwright.dev/docs/trace-viewer */
     trace: 'on-first-retry',
@@ -72,7 +72,7 @@ export default defineConfig({
   /* Run your local dev server before starting the tests */
   webServer: {
     command: 'npm start',
-    url: 'http://localhost:4200',
+    url: 'https://dynami-module-generator.onrender.com',
     reuseExistingServer: !process.env.CI,
   },
 });

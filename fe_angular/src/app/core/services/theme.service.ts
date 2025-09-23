27dbc943d4d4000a382e6b426a1d7b4da131ae17
uravashi0208 <zeel129patel@gmail.com>
2025-09-23 22:18:58 +0200

    changes in css

--- fe_angular/src/app/core/services/theme.service.ts
+++ fe_angular/src/app/core/services/theme.service.ts
@@ -6,7 +6,7 @@ import { effect } from '@angular/core';
   providedIn: 'root',
 })
 export class ThemeService {
-  public theme = signal<Theme>({ mode: 'dark', color: 'base', direction: 'ltr' });
+  public theme = signal<Theme>({ mode: 'light', color: 'green', direction: 'ltr' });
 
   constructor() {
     this.loadTheme();
